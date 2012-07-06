28a1bb84a21223a0e333ea8d3319d05c3f4a754e
JVendi <JVendi@D103260.newsint.internal>
2012-07-06 18:26:30 +0100

    sacsa

--- SelFw/src/test/java/MyExperiments/HttpClientPost.java
+++ SelFw/src/test/java/MyExperiments/HttpClientPost.java
@@ -1,22 +1,24 @@
 package MyExperiments;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HttpClientPost {
 
 	/**
 	 * @param args
+	 * @throws JSONException 
 	 * @throws UnsupportedEncodingException 
 	 */
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, JSONException{
 		// 
 		 
 		DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -32,7 +34,21 @@ public class HttpClientPost {
 		
 		HttpResponse response = httpClient.execute(postRequest);
 	 
-	System.out.println(response.getStatusLine().getStatusCode());
+     
+    	
+    	
+    	System.out.println(response.getStatusLine().getStatusCode());
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+    	
+    	String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+    	
+	 
+    	
 		 
 	}
 
