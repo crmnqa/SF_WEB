032ba4b8bb4eaacd03a697667b49cee4c0efbe12
jagspriya <jagspriya@gmail.com>
2012-07-27 17:30:37 +0100

    sdfgtr

--- SelFw/src/test/java/MyExperiments/NewsIntBaseClass.java
+++ SelFw/src/test/java/MyExperiments/NewsIntBaseClass.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,17 +15,19 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
 public class NewsIntBaseClass {
-
-
 	 
 		WebDriver driver;
 		String baseUrl;
-		DateFormat dateFormat = new SimpleDateFormat("_MM_dd_HH_mm_ss");
-		Date date = new Date();
-		String email1 = "webSel";
-		String email2 = "@zaqwsx.com" ;
-		String email = email1 + dateFormat.format(date)  + email2 ;
+		static DateFormat dateFormat = new SimpleDateFormat("_MM_dd_HH_mm_ss");
+		static Date date = new Date();
+		static String email1 = "websel";
+		static String email2 = "@zaqwsx.com" ;
+		static String email = email1 + dateFormat.format(date)  + email2 ;
 		
 		public void setup( ) throws InterruptedException {
 		 FirefoxProfile profile= new FirefoxProfile(); 
@@ -38,7 +42,7 @@ public class NewsIntBaseClass {
 		
 		 driver.manage().deleteAllCookies(); 
  
- 		driver.get("http://www.staging-thetimes.co.uk/tto/news/");  
+ 		driver.get("http://www.uat-thetimes.co.uk/tto/news/");  
  		Thread.sleep(5000);
  	    driver.findElement(By.xpath(".//*[@id='prenav']/ul[2]/li[2]/a")).click();
  		Thread.sleep(5000);
@@ -74,8 +78,58 @@ public class NewsIntBaseClass {
 		
 	}
 
+	  public static void custProfile(String content, String endpoint) throws JSONException{
+			   
+			   	String output =   restful(content, endpoint);
+			  
+			   String[ ] profile = new String[10];
+			    
+			      JSONObject jo = new JSONObject(output);   
+		   
+			  	  profile[0] = jo.get( "tenantId").toString();
+			  	  profile[1] = jo.get( "username").toString();
+			  	  profile[2] = jo.get( "externalId").toString();
+			  	   
+			     	JSONObject jos = new JSONObject(jo.get( "profileData").toString()); 
+			  	  
+			  	  profile[3] = jos.get( "lastName").toString();
+			  	  profile[4] = jos.get( "email").toString();
+			  	  profile[5] = jos.get( "displayName").toString();
+			  	  profile[6] = jos.get( "firstName").toString();
+			  	  profile[7] = jos.get( "iamId").toString();
+			  	  profile[8] = jos.get( "city").toString();
+			  	  profile[9] = jos.get( "country").toString();
+			  	  
+			   	  for ( int i = 0 ; i<=9; i ++)
+//		  		  
+		  	  {
+		  		
+		  		System.out.println(profile[i]);
+		  		  
+		  	  }
+			   		   
+		  }       
+		 	
 
+		  public static String restful(String content, String endpoint) {
+			  
+			  Client client = new Client();
+			   WebResource resource = client.resource(endpoint);
+			    
+			 		   ClientResponse response =
+			 		   resource.header("X-NI-signatureType", "2")
+			 		  .header("X-NI-apiVersion", "1")
+			 		  .header("X-NI-signatureHash", "dummy") 
+			 		  .header("X-NI-apiKey", "a3a0a84318e06c048a8bdb56c966f060")
+			 		  .type("application/json")
+			 		  .accept("application/json")
+			 		  .post(ClientResponse.class,content);
+					return response.getEntity(String.class);
 
+				 
+			   		   
+			  }
+			  
 
 
 
