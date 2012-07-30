002ba5338e34ea03cd11041c3a81596fc5f43e33
jagspriya <jagspriya@gmail.com>
2012-07-30 13:30:09 +0100

    Added Assertions

--- SelFw/src/test/java/MyExperiments/WebJourney.java
+++ SelFw/src/test/java/MyExperiments/WebJourney.java
@@ -1,45 +1,73 @@
 package MyExperiments;
 
 import MyExperiments.NewsIntBaseClass;
-import java.text.DateFormat;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.support.ui.Select;
+import org.json.JSONException;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class  WebJourney {
 	
  	NewsIntBaseClass NI = new NewsIntBaseClass();
+    String email = NI.email;	 
+     
+   
+    
+ 	//  String content = "{\"tenantId\":\"TNL\",\"username\":\"" + NI.email +"\"}" ;
+	  
+   	 String endpoint = "https://acs-uat.newsint.co.uk/profile/getUserProfile";
+   	 
  
-	@BeforeClass
-	public void setUp() throws Exception {
-
-		
-		NI.setup();
-		
-	}
-	
-	   
- 	@Test
-	public void testWebDriverWebJourney() throws Exception {
- 		 
-		  
- 		NI.webjourney(NI.email);
+   	 
+ 	@BeforeClass
+ 	public void setUp() throws Exception {
+ 	
+ 	NI.setup();
  		
+ 	}
+	
+    
+ 	@Test 
+	public void testWebDriverWebJourney1() throws Exception {
+ 	 	System.out.println("testWebDriverWebJourney1  " + email); 	  	  
+ 	NI.webjourney1(email);
+
+ 	}
+ 	
+ 	@Test 
+	public void zheckProfile() throws Exception {
+ 		  
+ 	 	System.out.println("Profiel check  " + email); 		
+ 		String content = "{\"tenantId\":\"TNL\",\"username\":\"" + email +"\"}" ;
+ 		String[] profile  = NewsIntBaseClass.custProfile(content,endpoint)  ;	
+ 	  	
+ 	  	 Assert.assertEquals( profile[0],"tnl","Tenant ID Verification");
+ 	  	 Assert.assertEquals( profile[1],NI.email,"User Name Verification");
+ 	  	 Assert.assertEquals( profile[2] != null,"External ID Verification");
+ 	  	Assert.assertEquals( profile[3],NI.lastName,"lastname Verification");
+ 	  	Assert.assertEquals( profile[4],NI.email,"email Verification");
+ 	  //	Assert.assertEquals( profile[5],,"DisplayName Verification");
+ 	  	Assert.assertEquals( profile[6], NI.firstName,"FirstName Verification");
+ 	  	Assert.assertEquals( profile[7] != null,"IAM ID Verification");
+ 	  	Assert.assertEquals( profile[8],NI.city,"email Verification");  	
+ 	  	//Assert.assertEquals( profile[9],NI.email,"Country Verification");
  	}
  	
  	
+ 	
+ 	@Test 
+	public void testWebDriverWebJourney2() throws Exception {
+ 	 	
+ 	 
+ 	 	System.out.println("testWebDriverWebJourney2  " + email); 	
+ 		
+ 	NI.webjourney2(email);
+
+ 	}
 		
 }
 
-
+ 
 	
  
  
